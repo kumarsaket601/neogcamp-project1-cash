1b50748b4db72ce697ce84c09f6770f36069a90b
saket kumar <kumarsaket601@gmail.com>
2021-08-03 13:43:51 +0530

    css update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var billAmount = document.querySelector("#billamount");
 var cashPaid = document.querySelector("#cashpaid");
 const noteToBeGiven = document.querySelectorAll(".notes");
 
-var log = console.log;
 
-var currency = [2000,500,100,50,20,10,1];
+
+const currency = [2000, 500, 100, 50, 20, 10, 1];
 
 function calculate(amountToBeReturn) {
 
@@ -13,7 +13,7 @@ function calculate(amountToBeReturn) {
 
     for (var i = 0; i < currency.length; i++) {
 
-        const numberOfNote = Math.trunc(amountToBeReturn / currency[i]);
+        var  numberOfNote = Math.trunc(amountToBeReturn / currency[i]);
         var amountToBeReturn  =  amountToBeReturn % currency[i];
 
 
@@ -21,7 +21,7 @@ function calculate(amountToBeReturn) {
          noteToBeGiven[i].innerText = numberOfNote;
 
         
-
+       
 
 
     }
@@ -32,20 +32,20 @@ function clickHandler() {
 
     if (billAmount.value > 0) {
 
-        if (cashPaid.value > billAmount.value) {
+        if (cashPaid.value >= billAmount.value) {
 
-            const amountToBeReturn = cashPaid.value - billAmount.value;
+            var amountToBeReturn = cashPaid.value - billAmount.value;
                          calculate(amountToBeReturn);
 
 
         }
          else{
-
+                
             }
 
     }
      else{
-
+            erroMessage();
       }
 
 
