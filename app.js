7bee6f63e9a307a1c67ef7e3075a394411770564
saket kumar <kumarsaket601@gmail.com>
2021-08-03 09:38:37 +0530

    update in js

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
 var btnCheck = document.querySelector("#btn-check");
 var billAmount = document.querySelector("#billamount");
 var cashPaid = document.querySelector("#cashpaid");
-var noteTobeGiven = document.querySelector("#notes");
+var noteToBeGiven = document.querySelector("#no-of-note");
 
 var log = console.log;
 
 var currency = [2000, 500, 100, 50, 20, 10, 1];
 
+function calculate(amountToBeReturn) {
 
 
-function clickHandler(){
 
-    if(billAmount.value >=  0){
+    for (var i = 0; i < currency.length; i++) {
 
-        if(cashPaid.value >= billAmount.value){
+        var numberOfNote = Math.trunc(amountToBeReturn / currency[i]);
+        var amountToBeReturn = amountToBeReturn % currency[i];
+
+        noteToBeGiven[i].innerText = numberOfNote;
+
+        
+
+
+
+    }
+}
+
+function clickHandler() {
+
+    if (billAmount.value >= 0) {
+
+        if (cashPaid.value >= billAmount.value) {
 
             var amountToBeReturn = cashPaid.value - billAmount.value;
-           // calculate(amountToBeReturn); 
-            for( var i = 0; i<currency.length; i++ ){
+            calculate(amountToBeReturn);
 
-                var numberOfNote = Math.trunc(amountToBeReturn / currency[i]);
-                var amountToBeReturn = amountToBeReturn % currency[i];
 
-                  var noteNumber = numberOfNote;
-
-                  noteTobeGiven[i].innerText = noteNumber;
-                
+        }
+         else{
 
             }
-            
-        }
-       // else{
 
-       // }
-        
     }
-    //else{
+     else{
 
-    //}
-       
- 
+      }
+
+
 };
 
 
 
-btnCheck.addEventListener("click", clickHandler)
+btnCheck.addEventListener('click', clickHandler)
